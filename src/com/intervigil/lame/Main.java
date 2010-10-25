e5a9be6c9a2bc9c6669c49fd43e9761f75fb44c3
Ethan Chen <intervigil@gmail.com>
2010-10-25 01:20:20 -0700

    add menu options

--- src/com/intervigil/lame/Main.java
+++ src/com/intervigil/lame/Main.java
@@ -21,13 +21,89 @@
 package com.intervigil.lame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.admob.android.ads.AdView;
+import com.intervigil.lame.helper.PreferenceHelper;
 
 public class Main extends Activity {
+	
+	private Boolean showAds;
+	private AdView ad;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        showAds = PreferenceHelper.getShowAds(Main.this);
+        ad = (AdView) findViewById(R.id.main_adview);
+        ad.setEnabled(showAds);
+    }
+    
+    @Override
+    protected void onStart() {
+        Log.i(getPackageName(), "onStart()");
+        super.onStart();
+    }
+    
+    @Override
+    protected void onResume() {
+    	Log.i(getPackageName(), "onResume()");
+    	super.onResume();
+    }
+    
+    @Override
+    protected void onPause() {
+    	Log.i(getPackageName(), "onPause()");
+    	super.onPause();
+    }
+    
+    @Override
+    protected void onStop() {
+    	Log.i(getPackageName(), "onStop()");
+    	super.onStop();
+    }
+    
+    @Override
+    protected void onDestroy() {
+    	Log.i(getPackageName(), "onDestroy()");
+    	super.onDestroy();
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.options:
+            	Intent preferencesIntent = new Intent(getBaseContext(), Preferences.class);
+            	startActivity(preferencesIntent);
+            	break;
+            case R.id.help:
+            	
+            	break;
+            case R.id.about:
+            	
+            	break;
+            case R.id.quit:
+            	finish();
+            	break;
+        }
+        return true;
     }
 }
