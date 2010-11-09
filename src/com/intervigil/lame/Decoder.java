a75737f4ad65b082211f4bfccb2e30dd27225e38
Ethan Chen <intervigil@gmail.com>
2010-11-08 21:08:42 -0800

    initial plumbing work done on decoder

--- src/com/intervigil/lame/Decoder.java
+++ src/com/intervigil/lame/Decoder.java
@@ -20,6 +20,54 @@
 
 package com.intervigil.lame;
 
-public class Decoder {
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import com.intervigil.wave.WaveWriter;
+
+public class Decoder {
+	private static final int MP3_BUFFER_SIZE = 1152;
+	private static final int INPUT_STREAM_BUFFER = 8192;
+	private WaveWriter waveWriter;
+	private File inFile;
+	private File outFile;
+
+	private BufferedInputStream in;
+
+	public Decoder(File in, File out) {
+		this.inFile = in;
+		this.outFile = out;
+	}
+
+	public void initialize() throws FileNotFoundException, IOException {
+		waveWriter = new WaveWriter(outFile, 44100, 2, 16);
+		waveWriter.createWaveFile();
+		in = new BufferedInputStream(new FileInputStream(inFile), INPUT_STREAM_BUFFER);
+		Lame.initDecoder();
+	}
+
+	public void decode() {
+		if (waveWriter != null && in != null) {
+
+		}
+	}
+
+	public void cleanup() {
+		try {
+			if (waveWriter != null) {
+				waveWriter.closeWaveFile();
+			}
+			if (in != null) {
+				in.close();
+			}
+		} catch (IOException e) {
+			// failed to close mp3 file or close output file
+			// TODO: actually handle an error here
+			e.printStackTrace();
+		}
+		Lame.closeDecoder();
+	}
 }
